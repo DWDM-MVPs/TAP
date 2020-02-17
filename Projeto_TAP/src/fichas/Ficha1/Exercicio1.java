ff560028e1541e26d90298a204c8bc4a00b209df
Milkenm <miguel.campos.johnny@gmail.com>
2020-02-17 18:33:59 +0000

    Ficha 1

---
+++ Projeto_TAP/src/fichas/Ficha1/Exercicio1.java
@@ -0,0 +1,47 @@
+package fichas.Ficha1;
+
+import java.util.Calendar;
+
+public class Exercicio1 {
+    public static class Pessoa{
+	public Pessoa(String nome, String nif, int anoNasc, String contacto) {
+	    this.nome = nome;
+	    this.nif = nif;
+	    this.anoNasc = anoNasc;
+	    this.contacto = contacto;
+	}
+
+        public String nome;
+        public String nif;
+        public int anoNasc;
+        private String contacto;
+
+	public int getAnoNasc(){return anoNasc;}
+        public String getContacto() {return contacto;}
+        public String getNif() {return nif;}
+        public String getNome() {return nome;}
+        public void setAnoNasc(int n) {this.anoNasc = n;}
+        public void setContacto(String contacto){this.contacto = contacto;}
+        public void setNif(String nif){this.nif = nif;}
+        public void setNome(String nome) {this.nome = nome;}
+        public int idade(){
+	    var year = Calendar.getInstance().get(Calendar.YEAR);
+	    return year - this.anoNasc;
+	}
+    }
+
+    public int CalcularIdade(Pessoa p){
+	var year = Calendar.getInstance().get(Calendar.YEAR);
+	return year - p.anoNasc;
+    }
+
+    public static void main(String[] args) {
+	Pessoa persona, p1, pessoaX;
+
+	persona = new Pessoa("persona", "123456789", 2002, "persona@gmail.com");
+	p1 = new Pessoa("p1", "123456789", 2003, "p1@gmail.com");
+	pessoaX = new Pessoa("pessoaX", "123456789", 2004, "pessoaX@gmail.com");
+
+	System.out.println("Nome: " + persona.getNome() + "\nNIF: " + persona.getNif() + "\nAno de Nascimento: " + persona.getAnoNasc()+" (" + persona.idade() + " anos)\nContacto: " + persona.getContacto());
+    }
+}
