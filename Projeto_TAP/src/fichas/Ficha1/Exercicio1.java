5e24ce99cdedeb93455dde0af9efb69d9b00912d
Milkenm <miguel.campos.johnny@gmail.com>
2020-02-18 11:57:03 +0000

    Novos métodos para a Ficha 1

--- Projeto_TAP/src/fichas/Ficha1/Exercicio1.java
+++ Projeto_TAP/src/fichas/Ficha1/Exercicio1.java
@@ -2,19 +2,17 @@ package fichas.Ficha1;
 
 public class Exercicio1 {
     public static void main(String[] args) {
-	Exercicio1_Pessoa persona, p1, pessoaX;
+	Exercicio1_Pessoa pessoa;
 
-	persona = new Exercicio1_Pessoa("persona", "123456789", 2002, "persona@gmail.com");
-	p1 = new Exercicio1_Pessoa("p1", "123456789", 2003, "p1@gmail.com");
-	pessoaX = new Exercicio1_Pessoa("abc", "abc", 000, "abc");
+	pessoa = new Exercicio1_Pessoa("abc", "abc", 000, "abc",0,0);
 
-	pessoaX.setNome("pessoaX");
-	pessoaX.setNif("123456789");
-	pessoaX.setAnoNasc(2004);
-	pessoaX.setContacto("pessoaX@gmail.com");
+	pessoa.setNome("pessoaX");
+	pessoa.setNif("123456789");
+	pessoa.setAnoNasc(2004);
+	pessoa.setContacto("pessoaX@gmail.com");
+	pessoa.setCreditos(20);
+	pessoa.setDespesas(10);
 
-	System.out.println("Nome: " + persona.getNome() + "\nNIF: " + persona.getNif() + "\nAno de Nascimento: " + persona.getAnoNasc()+" (" + persona.idade() + " anos)\nContacto: " + persona.getContacto());
-	System.out.println("Nome: " + p1.getNome() + "\nNIF: " + p1.getNif() + "\nAno de Nascimento: " + p1.getAnoNasc()+" (" + p1.idade() + " anos)\nContacto: " + p1.getContacto());
-	System.out.println("Nome: " + pessoaX.getNome() + "\nNIF: " + pessoaX.getNif() + "\nAno de Nascimento: " + pessoaX.getAnoNasc()+" (" + pessoaX.idade() + " anos)\nContacto: " + pessoaX.getContacto());
+	System.out.println("Nome: " + pessoa.getNome() + "\nNIF: " + pessoa.getNif() + "\nAno de Nascimento: " + pessoa.getAnoNasc()+" (" + pessoa.idade() + " anos)\nContacto: " + pessoa.getContacto() + "\nDinheiro: " + pessoa.dinheiro()+"€");
     }
 }
--- Projeto_TAP/src/fichas/Ficha1/Exercicio1_Pessoa.java
+++ Projeto_TAP/src/fichas/Ficha1/Exercicio1_Pessoa.java
@@ -3,28 +3,44 @@ package fichas.Ficha1;
 import java.util.Calendar;
 
 public class Exercicio1_Pessoa {
-    public Exercicio1_Pessoa(String nome, String nif, int anoNasc, String contacto) {
+    public Exercicio1_Pessoa(String nome, String nif, int anoNasc, String contacto, double creditos, double despesas) {
 	this.nome = nome;
 	this.nif = nif;
 	this.anoNasc = anoNasc;
 	this.contacto = contacto;
+	this.creditos = creditos;
+	this.despesas = despesas;
     }
 
     private String nome;
     private String nif;
     private int anoNasc;
     private String contacto;
+    private String morada;
+    private double creditos;
+    private double despesas;
 
     public int getAnoNasc(){return anoNasc;}
     public String getContacto() {return contacto;}
     public String getNif() {return nif;}
     public String getNome() {return nome;}
+    public String getMorada() {return morada;}
+    public double getCreditos(){return creditos;}
+    public double getDespesas(){return despesas;}
+
     public void setAnoNasc(int n) {this.anoNasc = n;}
     public void setContacto(String contacto){this.contacto = contacto;}
     public void setNif(String nif){this.nif = nif;}
     public void setNome(String nome) {this.nome = nome;}
+    public void setMorada(String morada){this.morada = morada;}
+    public void setCreditos(double creditos){this.creditos = creditos;}
+    public void setDespesas(double despesas){this.despesas = despesas;}
+
     public int idade(){
 	var year = Calendar.getInstance().get(Calendar.YEAR);
 	return year - this.anoNasc;
+    }
+    public double dinheiro(){
+        return creditos - despesas;
     }
 }
