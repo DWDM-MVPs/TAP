3df8c7c2f8847139cfe496f7fced0087762800d9
Milkenm <miguel.campos.johnny@gmail.com>
2020-02-18 11:42:44 +0000

    Testar algumas cenas

--- Projeto_TAP/src/fichas/Ficha1/Exercicio1.java
+++ Projeto_TAP/src/fichas/Ficha1/Exercicio1.java
@@ -6,7 +6,12 @@ public class Exercicio1 {
 
 	persona = new Exercicio1_Pessoa("persona", "123456789", 2002, "persona@gmail.com");
 	p1 = new Exercicio1_Pessoa("p1", "123456789", 2003, "p1@gmail.com");
-	pessoaX = new Exercicio1_Pessoa("pessoaX", "123456789", 2004, "pessoaX@gmail.com");
+	pessoaX = new Exercicio1_Pessoa("abc", "abc", 000, "abc");
+
+	pessoaX.setNome("pessoaX");
+	pessoaX.setNif("123456789");
+	pessoaX.setAnoNasc(2004);
+	pessoaX.setContacto("pessoaX@gmail.com");
 
 	System.out.println("Nome: " + persona.getNome() + "\nNIF: " + persona.getNif() + "\nAno de Nascimento: " + persona.getAnoNasc()+" (" + persona.idade() + " anos)\nContacto: " + persona.getContacto());
 	System.out.println("Nome: " + p1.getNome() + "\nNIF: " + p1.getNif() + "\nAno de Nascimento: " + p1.getAnoNasc()+" (" + p1.idade() + " anos)\nContacto: " + p1.getContacto());
